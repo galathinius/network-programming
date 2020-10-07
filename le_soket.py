b729b13e0d258326a578df77a0b465c736a0ff54
nela <anisoara.ionela@gmail.com>
2020-10-07 20:28:12 +0300

    cleaning up

--- le_soket.py
+++ le_soket.py
@@ -3,13 +3,14 @@ from threading import Thread
 import concurrent.futures as cf
 import queue
 import json, re
+import os  
 
-conn_q = queue.Queue(maxsize=20)
+conn_q = queue.Queue()
 
 HOST = '0.0.0.0'
 PORT = 5321
 
-threads = [] 
+Threads = [] 
 
 ma_file = open('parsed.json', "r")
 file_data = ma_file.read()
@@ -18,18 +19,20 @@ json_data = json.loads(file_data)
 def get_column(column, pattern=None):
     to_return = ''
     for person in json_data:
-        # print(person)
-        if column in json_data[person]:
+        if column in person:
             if pattern is not None:
-                if re.search(pattern, json_data[person][column]):
-                    to_return = f'{to_return}\n{json_data[person][column]}'
+                if re.search(pattern, person[column]):
+                    to_return = f'{to_return}\n{person[column]}'
             else:
-                # print(json_data[person][column])
-                to_return = f'{to_return}\n{json_data[person][column]}'
-    # print(f'getting col \n{to_return}')
+                to_return = f'{to_return}\n{person[column]}'
+    if to_return == '':
+        to_return = 'check your spelling, maybe there is no such column, or no data according to the pattern'
     return f'{to_return}\n\n'
 
 def parse_query(query):
+    help_string = """\nyou can use the following commands: 
+    getCol column - so that you get a column
+    getPat column pattern - so that you get a column filtered with the pattern\n\n"""
     
     query = query.replace('\n', '').replace('\r', '')
     words = query.split(' ')
@@ -38,8 +41,10 @@ def parse_query(query):
         return get_column(words[1])
     elif words[0] == 'getPat':
         return get_column(words[1], words[2])
+    elif words[0] == 'help':
+        return help_string
     else:
-        return 'sorry, we\'ve got no such command\n' 
+        return '\nsorry, we\'ve got no such command\n' + help_string
 
 def respond(con, adr):
     try:
@@ -49,33 +54,34 @@ def respond(con, adr):
                 data = con.recv(1024)
                 if not data:
                     break
-                # con.sendall(data)
                 print(data)
-                # print(data.decode('utf-8'))
                 response = parse_query(data.decode('utf-8'))
-                # print(f'sending resp{response}')
                 con.sendall(response.encode('utf-8'))
     except Exception:
         pass
     finally:
         con.close()
 
+def start_soket():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f'soket started, you can connect on the ip:')
+        cmd = "hostname -I |awk '{print $1}'"
+        os.system(cmd) 
+        print(f'using port {PORT} \n')
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-    s.bind((HOST, PORT))
-    
-    s.listen(5)
-    
-    while True:
-        
-        conn, addr = s.accept()
-        
-        x = Thread(target=respond, args=(conn, addr))
-        print(f'thread 1')
-        x.start()
-        threads.append(x)
-        
-    for t in threads: 
-        t.join() 
-    s.close()
+        while True:
+            conn, addr = s.accept()
+
+            x = Thread(target=respond, args=(conn, addr))
+            x.start()
+            Threads.append(x)
+            
+        for t in Threads: 
+            t.join() 
+        s.close()
+
+start_soket()
+# print(parse_query('getCol email'))
